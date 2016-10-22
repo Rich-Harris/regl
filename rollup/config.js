2f07dba108392d4daf765e645230cb22798c238b
Rich-Harris <richard.a.harris@gmail.com>
2016-10-22 17:13:54 -0400

    add jsnext:main

--- rollup/config.js
+++ rollup/config.js
@@ -2,8 +2,10 @@ var json = require('rollup-plugin-json')
 
 module.exports = {
   entry: 'regl.js',
-  dest: 'dist/regl.js',
-  format: 'umd',
+  targets: [
+    { dest: 'dist/regl.js', format: 'umd' },
+    { dest: 'dist/regl.es.js', format: 'es' }
+  ],
   moduleName: 'createREGL',
   plugins: [
     json()
--- rollup/config.unchecked.js
+++ rollup/config.unchecked.js
@@ -1,7 +1,11 @@
 var config = require('./config.js')
 var removeCheck = require('./plugins/remove-check.js')
 
-config.dest = 'dist/regl.unchecked.js'
+config.targets = [
+  { dest: 'dist/regl.unchecked.js', format: 'umd' },
+  { dest: 'dist/regl.unchecked.es.js', format: 'es' }
+];
+
 config.plugins.push(removeCheck())
 config.sourceMap = false
 
